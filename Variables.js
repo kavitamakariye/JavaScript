52eb5e5ee1d182d766147e9b86d0a9faae9553b8
kavitamakariye <kmakariye24@gmail.com>
2019-04-05 06:31:44 +0530

    variables in JavaScript

---
+++ Variables.js
@@ -0,0 +1,27 @@
+/*Varaible naming convention:
+1. Cannot be a reserved keyword
+2. Should be meaningful
+3. Cannot start with a number(1name)
+4. Cannot contain a space or hyphen (-)
+5. Are case-sensitive*/
+
+let firstName = 'Kavita';
+let lastName = 'Makariye';
+
+/*Variable are of two types of values 
+    1. Primitives/Value types
+        String,
+        Number, 
+        Boolean, 
+        undefined, 
+        null        
+     2. Reference Types
+        Object, 
+        Array,
+        Function  */    
+
+let name = 'Kavita'; //String Literal
+let age = 25; //Number Literal
+let isApproved = true; //Boolean Literal
+let firstName = undefined; // undefined Literal
+let selectedColor = null; // null Literal or typeOf(Object)
