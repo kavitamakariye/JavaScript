de6f6b0841eb16e3c0a4bc006bfc4454d3884cfb
kavitamakariye <kmakariye24@gmail.com>
2019-04-05 06:33:29 +0530

    datatypes in JavaScript

---
+++ DataTypes/ReferenceTypes/Arrays.js
@@ -0,0 +1,7 @@
+/* Arrays
+1. Different types of values can be stored in an array.
+2. Size of the array is dynamic i.e you can add elements in it anytime.
+*/
+let selectedColors = ['red', 'blue'];
+selectedColors[2] = 1;
+console.log(selectedColors.length);
---
+++ DataTypes/ReferenceTypes/Object.js
@@ -0,0 +1,10 @@
+let person = {
+    name: 'Kavita',
+    age: 25
+}; // Object Literal
+
+    //Dot Notation to access object property
+    person.name = "John";
+    //Bracket Notation to access object property
+    person['name'] = 'Mary';
+    console.log(person.name);
