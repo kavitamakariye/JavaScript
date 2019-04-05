1524879d5c6ffcc8e7340eaae2983c5f2cfa8b94
kavitamakariye <kmakariye24@gmail.com>
2019-04-05 06:34:02 +0530

    oops concepts in JavaScript

---
+++ OOPS/Encapsulation.js
@@ -0,0 +1,23 @@
+// Encapsulation
+
+// #### PROCEDURAL APPROACH #####
+
+let baseSalary = 30_000;
+let overtime = 10;
+let rate = 20;
+ 
+function getWage(baseSalary,overtime,rate){
+    return baseSalary + (overtime * rate);
+}
+
+// ##### OBJECT-ORIENTED APPROACH #####
+let employee = {
+    baseSalary : 30_000,
+    overtime : 10,
+    rate : 20,
+    getWage : function(){
+        return this.baseSalary + (this.overtime * this.rate);
+    }
+};
+
+employee.getWage();
