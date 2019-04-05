e8b5b70c126f3bed79ebffa0c8682d9316440384
kavitamakariye <kmakariye24@gmail.com>
2019-04-05 07:21:47 +0530

    modified the file

---
+++ OOPS/Object.js
@@ -0,0 +1,20 @@
+// ###### Object #####
+
+const circle = {
+    radius: 1,
+    location: {
+        x: 1,
+        y: 1
+    },
+    draw: function(){
+        console.log('draw');
+    }
+};
+
+circle.draw();
+
+/* To improve this above code we use Factory function or
+ Constructors */
+
+
+
