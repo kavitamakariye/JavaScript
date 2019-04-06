21462403fd23eeaa6b6903b913989af33f8d1f5c
kavitamakariye <kmakariye24@gmail.com>
2019-04-06 13:27:43 +0530

    added another approach to iterate object in EnumeratingProperties.js file

--- OOPS/EnumeratingProperties.js
+++ OOPS/EnumeratingProperties.js
@@ -1,4 +1,6 @@
-//Enumerating properties of Object, this can be done by using for-in loop.
+/*Enumerating properties of Object, this can be done by using for-in loop
+  or approach is use Object.keys(objName) method of object.
+*/
 
 function Circle(radius){
     this.radius = radius;
@@ -13,4 +15,15 @@ for(let key in circle){
     if(typeof circle[key] !== 'function')//checks the type of key and prints only properties and not methods
     //console.log(key);//return both properties and methods in an object
     console.log(key,circle[key]);// also prints members of object
+}
+
+//keys method approach
+
+const keys = Object.keys(circle);//stores the keys in array, but cannot separate properties and methods
+console.log(keys);
+
+//to check the properties in an object use in operator as shown below
+
+if('radius' in circle){
+    console.log('Circle has a radius');
 }
